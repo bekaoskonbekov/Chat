cf118ab9c7fecb3cee2e49e9b1b9e0936765d635
bekaoskonbekov <bekaoskonbekov1408@gmail.com>
2023-06-30 12:38:53 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:chat/screens/mobile_layouyt_screen.dart';
+import 'package:chat/screens/web_mobile_screen.dart';
+import 'package:chat/utils/responsive_layout.dart';
+import 'package:flutter/material.dart';
+
+import 'colors.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Whatsapp UI',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: backgroundColor,
+      ),
+      home: const ResponsiveLayout(
+        mobileScreenLayout: MobileLayoutScreen(),
+        webScreenLayout: WebLayoutScreen(),
+      ),
+    );
+  }
+}
